4ba1067e15345417ce63bf45371f281855224f70
Luigi <l.avila.uvg@gmail.com>
2023-10-27 11:59:23 -0400

    feat: click on menu actions test added

---
+++ app/src/androidTest/java/com/cursosant/inventorybase/mainModule/view/MainActivityTest.kt
@@ -0,0 +1,31 @@
+package com.cursosant.inventorybase.mainModule.view
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import com.cursosant.inventorybase.R
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest{
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun actionBar_menuItemClick_returnMessage(){
+        onView(withId(R.id.recyclerView)).perform(click())
+
+        onView(withId(R.id.action_history)).perform(click())
+
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText(R.string.menu_msg_history)))
+    }
+}
